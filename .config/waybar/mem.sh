cdaa8455f89e689a5ac57b280b8575ebe174aafb
Funami580 <Funami580@users.noreply.github.com>
2022-01-04 00:31:19 +0100

    waybar: custom memory: fixed padding with space

--- .config/waybar/mem.sh
+++ .config/waybar/mem.sh
@@ -5,4 +5,4 @@ mem_used=$(LC_MESSAGES=C free -m | awk '$1 == "Mem:" { print ($2-$7)/1024 }' | L
 mem_total=$(LC_MESSAGES=C free -m | awk '$1 == "Mem:" { print $2/1024 }' | LC_NUMERIC=C xargs printf "%.1f")
 swap_used=$(LC_MESSAGES=C free -m | awk '$1 == "Swap:" { print $3/1024 }' | LC_NUMERIC=C xargs printf "%.1f")
 swap_total=$(LC_MESSAGES=C free -m | awk '$1 == "Swap:" { print $2/1024 }' | LC_NUMERIC=C xargs printf "%.1f")
-echo '{"text": "'"${mem_percent}"'", "tooltip": "Mem: '"$(echo -e "${mem_percent}% ${mem_used}GiB/${mem_total}GiB\n${swap_percent}% ${swap_used}GiB/${swap_total}GiB" | column -t -R 1,2 | sed '2s/^/Swp: /' | head -c -1 | tr '\n' '*' | sed 's/*/\\n/g')"'" }'
+echo '{"text": "'"${mem_percent}"'", "tooltip": "Mem: '"$(echo -e "${mem_percent}%|${mem_used}GiB/${mem_total}GiB\n${swap_percent}%|${swap_used}GiB/${swap_total}GiB" | column -t -s '|' -R 1,2 | sed '2s/^/Swp: /' | head -c -1 | tr '\n' '*' | sed 's/*/\\n/g')"'" }'
