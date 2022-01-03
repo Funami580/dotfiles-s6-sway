abcad5ee6a9824bba0f0a4eefd01bb85f38bdb68
Funami580 <Funami580@users.noreply.github.com>
2022-01-03 15:54:02 +0100

    wezterm: use ScrollToTop and ScrollToBottom

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,8 +17,8 @@ return {
     {key="Numpad0", mods="CTRL", action="ResetFontSize"},
     {key="PageUp", mods="", action=wezterm.action{ScrollByPage=-1}},
     {key="PageDown", mods="", action=wezterm.action{ScrollByPage=1}},
-    {key="Home", mods="", action=wezterm.action{ScrollByLine=-15000}},
-    {key="End", mods="", action=wezterm.action{ScrollByLine=15000}},
+    {key="Home", mods="", action="ScrollToTop"},
+    {key="End", mods="", action="ScrollToBottom"},
     {key="f", mods="CTRL|SHIFT", action=wezterm.action{Search={CaseInSensitiveString=""}}},
   },
   window_padding = {
@@ -28,10 +28,10 @@ return {
     bottom = 0,
   },
   selection_word_boundary = " \t\n{}[]()\"'`.,;:!?",
+  audible_bell = "Disabled",
   exit_behavior = "Close",
   hide_tab_bar_if_only_one_tab = true,
   enable_scroll_bar = true,
   scrollback_lines = 10000,
   check_for_updates = false,
-  enable_wayland = true,
 }
