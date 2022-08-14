ee15a88e526f37589e8d3f1815d7705a893bfe23
Funami580 <Funami580@users.noreply.github.com>
2022-08-14 19:39:55 +0200

    sway: change window title when muting a specific window

--- .config/sway/mute_window.sh
+++ .config/sway/mute_window.sh
@@ -1,30 +1,40 @@
 #!/bin/sh
+
+# Get pid of the focused window
 focused_pid=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true).pid')
 
 if [[ -z $focused_pid ]]; then
-    exit
+  # Nothing focused, so exit
+  exit
 fi
 
-sink=$(pactl list sink-inputs | awk -v pid=$focused_pid '$1 == "Sink" && $2 == "Input" {idx = $3} $1 == "application.process.id" && $3 == "\""pid"\"" {print idx; exit}' | tr -d '#')
+# Finds sinks related to the pid
+#
+# First entry: whether the first sink is currently muted
+# Other entries: sink ids
+muted_and_sinks=( $(
+  pactl list sink-inputs |
+  awk -v pid=$focused_pid \
+    'BEGIN { count = 0 }
+     $1 == "Sink" && $2 == "Input" { sink = $3 }
+     $1 == "Mute:" { muted = $2 }
+     $1 == "application.process.id" && $3 == "\""pid"\"" { if (count == 0) { print muted; count++ } print sink }' |
+  tr -d '#'
+) )
 
-if [[ -z $sink ]]; then
-    exit
+if [[ ${#muted_and_sinks[@]} -lt 2 ]]; then
+  # No sinks available, so exit
+  exit
 fi
 
-pactl set-sink-input-mute $sink toggle
-
-exit
-# Future: TODO
-is_muted=$(pactl list sink-inputs | awk -v pid=$focused_pid '$1 == "Mute:" {idx = $2} $1 == "application.process.id" && $3 == "\""pid"\"" {print idx; exit}')
-
-if [[ $is_muted = "no" ]]; then
-    swaymsg for_window "[pid=$focused_pid]" 'title_format "%title"'
-else
-    swaymsg for_window "[pid=$focused_pid]" 'title_format " %title"'
-fi
-
-while test -d /proc/$focused_pid; do
-    sleep 1
+# Toggle mute for all sinks that are related to that pid
+for ((i = 1; i < ${#muted_and_sinks[@]}; ++i)); do
+  pactl set-sink-input-mute ${muted_and_sinks[$i]} toggle &
 done
 
-swaymsg for_window "[pid=$focused_pid]" 'title_format "%title"'
+# Change window title
+if [[ ${muted_and_sinks[0]} = "no" ]]; then
+  swaymsg "[pid=\"$focused_pid\"] title_format \" %title\""
+else
+  swaymsg "[pid=\"$focused_pid\"] title_format \"%title\""
+fi
