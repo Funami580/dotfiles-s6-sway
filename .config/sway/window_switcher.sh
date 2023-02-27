20c19222ff72bcb5983a2e74a490ddd928b4119b
Funami580 <Funami580@users.noreply.github.com>
2023-02-27 08:30:44 +0100

    sway: window switcher: use swayr (why re-invent the wheel?)

--- .config/sway/window_switcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-swaymsg -t get_tree | jq  -r  '.nodes[].nodes[]  |  if  .nodes  then  [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[] and (.type=="con" or .type=="floating_con")) | ((.id | tostring) + ". " + .name)' | wofi -i -p '' -M multi-contains -d | {
-    read -r index name
-    id=$(echo -n "${index}" | sed 's/\.//')
-    swaymsg "[con_id=$id]" focus
-}
