34ceac1c2f003ae79d02e20f76eab47c26b34649
Funami580 <Funami580@users.noreply.github.com>
2023-02-27 07:38:35 +0100

    sway: window switcher: fix crashes due to %s being unescaped

--- .config/sway/window_switcher.sh
+++ .config/sway/window_switcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-swaymsg -t get_tree | jq  -r  '.nodes[].nodes[]  |  if  .nodes  then  [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[] and (.type=="con" or .type=="floating_con")) | ((.id | tostring) + " " +
-.name)' | wofi -i -p '' -M multi-contains -r $'cat <<EOF | cut -f 1 -d " " --complement | head -c -1\n%s\nEOF\n' -d | {
-    read -r id name
+swaymsg -t get_tree | jq  -r  '.nodes[].nodes[]  |  if  .nodes  then  [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[] and (.type=="con" or .type=="floating_con")) | ((.id | tostring) + ". " + .name)' | wofi -i -p '' -M multi-contains -d | {
+    read -r index name
+    id=$(echo -n "${index}" | sed 's/\.//')
     swaymsg "[con_id=$id]" focus
 }
